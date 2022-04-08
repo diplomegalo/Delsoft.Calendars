88627efa46fb77b8680d08cdf4a622f1b502c09e
DELSAUT Pierre-arnaud <dlsprr@forem.be>
2022-04-08 15:24:22 +0200

    fix naming from holidays => calendars

--- Delsoft.Calendars.Belgian/Holidays/NationalHolidays.cs
+++ Delsoft.Calendars.Belgian/Holidays/NationalHolidays.cs
@@ -6,5 +6,4 @@ public static class NationalHolidays
 {
     public static DateTime LaborDay(this HolidaysCalendar holidaysCalendar) => new(holidaysCalendar.Year, 5, 1);
     public static DateTime NationalHoliday(this HolidaysCalendar holidaysCalendar) => new(holidaysCalendar.Year, 7, 21);
-
 }
